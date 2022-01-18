94f1fff6ca53b1854e9e455fc5806c2678312457
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-01-18 19:26:13 -0300

    defalt

---
+++ api.js
@@ -0,0 +1,14 @@
+const fetchCurrency = async (currency) => {
+  const response = await fetch(`https://api.exchangerate.host/latest?base=${currency}`);
+  const object = await response.json();
+
+  handleRates(object.rates);
+  handleBaseCurrency(object.base);
+
+}
+
+if (typeof(module) !== 'undefined') {
+  module.exports = {
+    fetchCurrency,
+  };
+}
